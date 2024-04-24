151ef821f7a2b49767f5ae81316a52a9be28dedc
Brianoyaro <brianoyaro2000@yahoo.com>
2024-04-24 10:01:48 +0300

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     return {status: 200, body: 'success'}})
   .then(console.log('Got a response from the API'))
-  .catch(() => {return})
+  .catch(() => {return new Error()})
   /*if (promise.then()){
     console.log('Got a response from the API')
     return {status: 200, body: 'success'}
