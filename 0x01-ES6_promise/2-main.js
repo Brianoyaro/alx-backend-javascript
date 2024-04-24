f49172c3ef8d5678f2599ee1db8b2a71906487ef
Brianoyaro <brianoyaro2000@yahoo.com>
2024-04-24 13:24:41 +0300

    tast 2

--- 0x01-ES6_promise/2-main.js
+++ 0x01-ES6_promise/2-main.js
@@ -1,5 +1,4 @@
 import handleResponseFromAPI from "./2-then";
 
-//const promise = Promise.resolve();
-const promise = Promise.reject();
+const promise = Promise.resolve();
 handleResponseFromAPI(promise);
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }))
-    .then(console.log('Got a response from the API'))
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  })
     .catch(() => new Error());
   /* if (promise.then()){
     console.log('Got a response from the API')
