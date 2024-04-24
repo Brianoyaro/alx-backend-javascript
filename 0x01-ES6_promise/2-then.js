fcca3676a33578a8ca31155f74f6133fd5ca1c9b
Brianoyaro <brianoyaro2000@yahoo.com>
2024-04-24 12:30:21 +0300

    uploading all tasks answers - pending checking

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    return {status: 200, body: 'success'}})
-  .then(console.log('Got a response from the API'))
-  .catch(() => {return new Error()})
-  /*if (promise.then()){
+  promise.then(() => ({ status: 200, body: 'success' }))
+    .then(console.log('Got a response from the API'))
+    .catch(() => new Error());
+  /* if (promise.then()){
     console.log('Got a response from the API')
     return {status: 200, body: 'success'}
   } else {
     return {}
-  }*/
-;}
+  } */
+}
