f33c6bd04417a1d633b331e656a296768c1c6bb3
Brianoyaro <brianoyaro2000@yahoo.com>
2024-04-30 15:53:51 +0300

    task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,14 +6,17 @@ interface Teacher {
     location: string,
     [key: string]: any,
 }
+
 interface Directors extends Teacher {
     numberOfReports: number,
 }
-const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-  };
-  console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string,
+}
+
+const printTeacher: printTeacherFunction = function(firstName: string, lastName: string): string{
+    return `${firstName[0]}. ${lastName}`
+}; 
+
+console.log(printTeacher("John", "Doe"))
