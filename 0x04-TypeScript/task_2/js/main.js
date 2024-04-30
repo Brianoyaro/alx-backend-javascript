f9d36b2fdfc4ab414daec042367e7c2ca46f3e5c
Brianoyaro <brianoyaro2000@yahoo.com>
2024-04-30 17:52:13 +0300

    task 6

--- 0x04-TypeScript/task_2/js/main.js
+++ 0x04-TypeScript/task_2/js/main.js
@@ -28,6 +28,16 @@ function createEmployee(salary) {
         return new Director();
     }
 }
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+function isDirector(employee) {
+    return employee instanceof Director;
+}
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    else {
+        return employee.workTeacherTasks();
+    }
+}
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -36,6 +36,17 @@ function createEmployee(salary: number|string): Director|Teacher {
     }
 }
 
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+function isDirector(employee: any): boolean {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: any): any {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
