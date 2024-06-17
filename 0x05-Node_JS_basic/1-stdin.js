c4f83f87eae83f5cb93dca29b456d9d929877bb1
Brianoyaro <brianoyaro2000@yahoo.com>
2024-06-17 20:21:25 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,9 +3,11 @@ process.stdin.setEncoding('utf-8')
 process.stdin.on('readable', function() {
     const name = process.stdin.read();
     if (name !== null) {
-        process.stdout.write(`Your name is ${name}`);
+        // process.stdout.write(`Your name is ${name}`);
+        console.log(`Your name is ${name}`);
     }
 })
 process.stdin.on('end', function() {
-    process.stdout.write('This important software is now closing')
+    // process.stdout.write('This important software is now closing')
+    console.log('This important software is now closing')
 })
