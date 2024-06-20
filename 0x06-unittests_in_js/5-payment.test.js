56fe0b69f4e35ce5ab85c8295409634e3d7437e3
Brianoyaro <brianoyaro2000@yahoo.com>
2024-06-20 13:51:14 +0300

    task 5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -5,17 +5,19 @@ const sinon = require('sinon')
 describe('describe message', function() {
     let mySpy;
     beforeEach(function() {
-        mySpy = sinon.spy(sendPaymentRequestToAPI);
+        mySpy = sinon.spy(console, 'log');
     })
     afterEach(function() {
         mySpy.restore();
     })
     it('sending 100 and 20, expects 120', function() {
-      sendPaymentTequestToAPI(100, 20);
+      sendPaymentRequestToAPI(100, 20);
       expect(mySpy.calledOnce).to.be.true;
+      expect(mySpy.calledWith('The total is: 120')).to.be.true;
     });
     it('sending 10 and 10, expects 20', function() {
-      sendPaymentTequestToAPI(10, 10);
+      sendPaymentRequestToAPI(10, 10);
       expect(mySpy.calledOnce).to.be.true;
+      expect(mySpy.calledWith('The total is: 20')).to.be.true;
     });
 });
