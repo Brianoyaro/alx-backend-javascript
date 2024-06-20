839287026b440b518dfbaf6203c043bf01d14ee5
Brianoyaro <brianoyaro2000@yahoo.com>
2024-06-20 08:47:27 +0300

    task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,7 +4,7 @@ const calculateNumber = require("./6-payment_token.js");
 describe("Async test with done", function() {
   it("asserts data received from api", function(done) {
     getPaymentTokenFromAPI(true).then(response => {
-      expect(reponse).to.equal({data: 'Successful response from the API' });
+      expect(reponse).to.deep.equal({data: 'Successful response from the API' });
       done();
     }).catch(err => {
       done(err);
