3f217da4e683098e72586d41afed490170607a1e
Brianoyaro <brianoyaro2000@yahoo.com>
2024-06-20 19:27:23 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
-console.log('Welcome to Holberton School, what is your name?')
-process.stdin.setEncoding('utf-8')
-process.stdin.on('readable', function() {
-    let name = process.stdin.read();
-    if (name !== null) {
-        // process.stdout.write(`Your name is ${name}`);
-	name = name.trim();
-        console.log(`Your name is: ${name}`);
-    }
-})
-process.stdin.on('end', function() {
-    // process.stdout.write('This important software is now closing')
-    console.log('This important software is now closing')
-})
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', () => {
+  let name = process.stdin.read();
+  if (name !== null) {
+    name = name.trim();
+    console.log(`Your name is: ${name}`);
+  }
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
