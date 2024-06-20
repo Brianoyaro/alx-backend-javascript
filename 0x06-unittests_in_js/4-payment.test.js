dc73cca904b07069b719433540f9af545d5d8501
Brianoyaro <brianoyaro2000@yahoo.com>
2024-06-20 13:47:17 +0300

    task 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -2,7 +2,7 @@ const chai = require("chai");
 const expect = chai.expect;
 // import sinon
 const sinon = require("sinon");
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 const utils = require('./utils.js');
 
 
@@ -11,11 +11,11 @@ describe('stub', function() {
     let mySpy = sinon.spy(console, 'log');
     let myStub = sinon.stub(utils, 'calculateNumber')
     myStub.returns(10)
-    sendRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
     expect(myStub.calledWith('SUM', 100, 20)).to.be.true;
     // mySpy should verify console.log from sendPaymentToApi logs 'The total is 10
     expect(mySpy.calledWith('The total is: 10')).to.be.true;
-    mySpy.restore()
-    myStub.restore()
+    mySpy.restore();
+    myStub.restore();
   });
 });
